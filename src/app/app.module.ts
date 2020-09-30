6d2357b463102f0d792dd8b4f3920bba4e90a512
Đỗ Tuấn Anh <65461336+Nino610@users.noreply.github.com>
2020-09-30 16:37:41 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { AdressComponent } from './adress/adress.component';
+import { FooterComponent } from './footer/footer.component';
+import { Header2Component } from './header2/header2.component';
+import { Sidebar2Component } from './sidebar2/sidebar2.component';
+import { Main2Component } from './main2/main2.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    AdressComponent,
+    FooterComponent,
+    Header2Component,
+    Sidebar2Component,
+    Main2Component,
+    ProductComponent
   ],
   imports: [
     BrowserModule
